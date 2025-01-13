39c7c124d159c578ab9baf19278cbc5da390553b
Fauve den Brok <fauvebrok@gmail.com>
2025-01-13 22:58:58 +0100

    Added bookArray

--- BookAPI/Controllers/BooksController.cs
+++ BookAPI/Controllers/BooksController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookAPI.Controllers
 {
@@ -6,6 +7,18 @@ namespace BookAPI.Controllers
     [ApiController]
     public class BooksController : ControllerBase
     {
+        private Book[] _books = new Book[]
+            { 
+                new Book { Id = 1, Author = "J.H.J Terstegge", Title = "Grip op de AVG" },
+                new Book { Id = 2, Author = "Hans Baars, Jule Hintzbergen, Kees Hintzbergen", Title = "Basiskennis informatiebeveiliging" },
+                new Book { Id = 3, Author = "Roel Grit", Title = "Informatiemanagement" },
+                new Book { Id = 4, Author = "Jos Warmer, Anneke Kleppe", Title = "Praktisch UML" }
+            };
 
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetBooks()
+        {
+            return Ok(_books);
+        }
     }
 }
