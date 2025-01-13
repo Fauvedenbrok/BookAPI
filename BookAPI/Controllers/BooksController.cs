9b0e2ebfbdc169caa173e0cad951e114f590a507
Fauve den Brok <fauvebrok@gmail.com>
2025-01-13 21:47:27 +0100

    Add project files.

---
+++ BookAPI/Controllers/BooksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[Controller]")] //https://localhost:7152/api/books
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+
+    }
+}
